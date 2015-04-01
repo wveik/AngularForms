977a97e825b34c7317beeeface531f3c569724f6
wveik <defender>
2015-04-01 14:18:05 +0300

    Небольшая работа в контроллере

--- AngularFormsWithAspMvc/app/EmployeeForm/efController.js
+++ AngularFormsWithAspMvc/app/EmployeeForm/efController.js
@@ -1,6 +1,8 @@
 ﻿angularFormsApp.controller('efController',
-    function EmployeeFormController($scope, DataService) {
+    function EmployeeFormController($scope, $window, DataService) {
         $scope.employee = DataService.employee;
+
+        $scope.editableEmployee = angular.copy($scope.employee);
 
         $scope.departments = [
                 "Инжинер",
@@ -10,7 +12,12 @@
         ];
 
         $scope.submitForm = function () {
-            
+            $scope.employee = angular.copy($scope.editableEmployee);
+            $window.history.back();
+        }
+
+        $scope.cancelForm = function () {
+
         }
     }
 ); 
